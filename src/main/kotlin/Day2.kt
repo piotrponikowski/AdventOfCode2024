9933636ff48220cc34a1eb066a8168fa23ef1e34
Piotr Ponikowski <piotr.ponikowski@source-corp.com>
2024-12-02 06:18:40 +0100

    day 2

--- src/main/kotlin/Day2.kt
+++ src/main/kotlin/Day2.kt
@@ -1,36 +1,20 @@
 class Day2(input: List<String>) {
+
+    private val reports = input.map { report -> report.split(" ").map { level -> level.toInt() } }
+
+    fun part1() = reports.count { report -> isSafe(report) }
+
+    fun part2() = reports.map { report -> splitLevels(report) }
+        .count { splitReports -> splitReports.any { report -> isSafe(report) } }
+
+    private fun isSafe(report :List<Int>) = isIncreasing(report) || isDecreasing(report)
     
-    private val reports = input.map { a -> a.split(" ").map { b -> b.toInt() } }
-    
-    fun part1() {
-        println(reports)
-        
-        val result = reports.map { isIncreasing(it) || isDecreasing(it) }.filter { it }.count()
-        println(result)
-    }
+    private fun isIncreasing(report: List<Int>) = report.windowed(2)
+        .all { (levelPrev, levelNext) -> levelNext > levelPrev && levelNext - levelPrev in 1..3 }
 
-    fun part2() {
-        println(reports)
-        println(split(reports[0]))
+    private fun isDecreasing(report: List<Int>) = report.windowed(2)
+        .all { (levelPrev, levelNext) -> levelPrev > levelNext && levelPrev - levelNext in 1..3 }
 
-        val result = reports.map { split(it) }.map { l -> l.any {  isIncreasing(it) || isDecreasing(it) }}.filter { it }.count()
-        println(result) 
-    }
-    
-    private fun isIncreasing(numbers: List<Int>) = numbers.windowed(2).all { (a, b) -> b > a && b-a in 1..3 }
-
-    private fun isDecreasing(numbers: List<Int>) = numbers.windowed(2).all { (a, b) -> a > b && a-b in 1..3 }
-
-    private fun split(numbers: List<Int>) = numbers.indices.map { index -> numbers.filterIndexed { index2, i -> index != index2 } }
-}
-
-fun main() {
-    val realInput = readLines("day2.txt")
-    val exampleInput = readLines("day2.txt", true)
-
-    val r1 = Day2(exampleInput).part2()
-    println(r1)
-
-    val r2 = Day2(realInput).part2()
-    println(r2)
+    private fun splitLevels(report: List<Int>) =
+        report.indices.map { index -> report.filterIndexed { other, _ -> index != other } }
 }
