7214473abd4e573fa8dcc2dcc75183494bb14560
Piotr Ponikowski <piotr.ponikowski@source-corp.com>
2024-12-04 06:40:22 +0100

    day 4 cleanup

--- src/main/kotlin/Day4.kt
+++ src/main/kotlin/Day4.kt
@@ -1,32 +1,27 @@
 class Day4(input: List<String>) {
 
+    private val edges = listOf(Point(-1, 0), Point(1, 0), Point(0, -1), Point(0, 1))
+    private val corners = listOf(Point(-1, -1), Point(1, -1), Point(-1, 1), Point(1, 1))
+    private val directions = edges + corners
+    
     private val board = input
         .flatMapIndexed { y, line -> line.mapIndexed { x, symbol -> Point(x, y) to symbol } }.toMap()
 
     fun part1() = board.keys
-        .sumOf { point -> Direction.entries.count { direction -> checkPhrase(point, direction, "XMAS") } }
+        .sumOf { point -> directions.count { direction -> checkPhrase(point, direction, "XMAS") } }
 
     fun part2() = board.keys
-        .map { point -> Direction.corners().count { direction -> checkPhrase(point - direction, direction, "MAS") } }
+        .map { point -> corners.count { direction -> checkPhrase(point - direction, direction, "MAS") } }
         .count { count -> count == 2 }
 
-    private fun checkPhrase(start: Point, direction: Direction, phrase: String): Boolean {
+    private fun checkPhrase(start: Point, direction: Point, phrase: String): Boolean {
         val points = (0..phrase.length - 2).runningFold(start) { point, _ -> point + direction }
         val letters = points.map { point -> board[point] }
         return letters.filterIndexed { index, letter -> letter == phrase[index] }.size == phrase.length
     }
 
     data class Point(val x: Int, val y: Int) {
-        operator fun plus(other: Direction) = Point(x + other.x, y + other.y)
-        operator fun minus(other: Direction) = Point(x - other.x, y - other.y)
-    }
-
-    enum class Direction(val x: Int, val y: Int) {
-        L(-1, 0), R(1, 0), U(0, -1), D(0, 1),
-        LU(-1, -1), LD(-1, 1), RU(1, -1), RD(1, 1);
-
-        companion object {
-            fun corners() = listOf(LU, LD, RU, RD)
-        }
+        operator fun plus(other: Point) = Point(x + other.x, y + other.y)
+        operator fun minus(other: Point) = Point(x - other.x, y - other.y)
     }
 }
