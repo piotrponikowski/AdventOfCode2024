01812530ae47fd021341974a5bc8a00498702abd
Piotr Ponikowski <piotr.ponikowski@source-corp.com>
2024-12-04 06:20:41 +0100

    day 4 tests

--- src/test/kotlin/Day4Test.kt
+++ src/test/kotlin/Day4Test.kt
@@ -5,24 +5,24 @@ class Day4Test : FunSpec({
 
     val realInput = readLines("day4.txt")
     val exampleInput = readLines("day4.txt", true)
-    
+
     context("Part 1") {
         test("should solve example") {
-            Day4(exampleInput).part1() shouldBe 1
+            Day4(exampleInput).part1() shouldBe 18
         }
-        
+
         test("should solve real input") {
-            Day4(realInput).part1() shouldBe 1
+            Day4(realInput).part1() shouldBe 2639
         }
     }
 
     context("Part 2") {
         test("should solve example") {
-            Day4(exampleInput).part2() shouldBe 2
+            Day4(exampleInput).part2() shouldBe 9
         }
-        
+
         test("should solve real input") {
-            Day4(realInput).part2() shouldBe 2
+            Day4(realInput).part2() shouldBe 2005
         }
     }
 })
