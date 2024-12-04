6ce64c25115299b9cfdf5eb9ab284c14fdcf81fc
Piotr Ponikowski <piotr.ponikowski@source-corp.com>
2024-12-04 06:35:07 +0100

    day 4 cleanup

--- src/main/kotlin/Day4.kt
+++ src/main/kotlin/Day4.kt
@@ -4,38 +4,17 @@ class Day4(input: List<String>) {
         .flatMapIndexed { y, line -> line.mapIndexed { x, symbol -> Point(x, y) to symbol } }.toMap()
 
     fun part1() = board.keys
-        .sumOf { point -> Direction.entries.count { direction -> check(point, direction) } }
+        .sumOf { point -> Direction.entries.count { direction -> check(point, direction, "XMAS") } }
 
     fun part2() = board.keys
-        .map { point -> Direction.corners().count { direction -> check2(point, direction) } }
+        .map { point -> Direction.corners().count { direction -> check(point - direction, direction, "MAS") } }
         .count { count -> count == 2 }
 
-    private fun check(start: Point, direction: Direction): Boolean {
-        val xp = start
-        val mp = xp + direction
-        val ap = mp + direction
-        val sp = ap + direction
-
-        val x = board[xp]
-        val m = board[mp]
-        val a = board[ap]
-        val s = board[sp]
-
-        return x == 'X' && m == 'M' && a == 'A' && s == 'S'
+    private fun check(start: Point, direction: Direction, phrase: String): Boolean {
+        val points = (0..phrase.length - 2).runningFold(start) { point, _ -> point + direction }
+        val letters = points.map { point -> board[point] }
+        return letters.filterIndexed { index, letter -> letter == phrase[index] }.size == phrase.length
     }
-
-    private fun check2(start: Point, direction: Direction): Boolean {
-        val mp = start - direction
-        val ap = mp + direction
-        val sp = ap + direction
-
-        val m = board[mp]
-        val a = board[ap]
-        val s = board[sp]
-
-        return m == 'M' && a == 'A' && s == 'S'
-    }
-
 
     data class Point(val x: Int, val y: Int) {
         operator fun plus(other: Direction) = Point(x + other.x, y + other.y)
@@ -51,15 +30,4 @@ class Day4(input: List<String>) {
             fun corners() = listOf(LU, LD, RU, RD)
         }
     }
-}
-
-fun main() {
-    val realInput = readLines("day4.txt")
-    val exampleInput = readLines("day4.txt", true)
-
-    val r1 = Day4(exampleInput).part2()
-    println(r1)
-
-    val r2 = Day4(realInput).part2()
-    println(r2)
 }
