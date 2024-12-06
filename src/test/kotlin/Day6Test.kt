3c2c6d0381e8f3a14bc370d24fa8ea1f524d09d7
Piotr Ponikowski <piotr.ponikowski@source-corp.com>
2024-12-06 06:33:46 +0100

    day 6

---
+++ src/test/kotlin/Day6Test.kt
@@ -0,0 +1,28 @@
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class Day6Test : FunSpec({
+
+    val realInput = readLines("day6.txt")
+    val exampleInput = readLines("day6.txt", true)
+
+    context("Part 1") {
+        test("should solve example") {
+            Day6(exampleInput).part1() shouldBe 1
+        }
+
+        test("should solve real input") {
+            Day6(realInput).part1() shouldBe 1
+        }
+    }
+
+    context("Part 2") {
+        test("should solve example") {
+            Day6(exampleInput).part2() shouldBe 2
+        }
+
+        test("should solve real input") {
+            Day6(realInput).part2() shouldBe 2
+        }
+    }
+})
