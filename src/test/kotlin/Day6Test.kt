58efc1fe9ff5e985e3bc54f5c31a177ff320780f
Piotr Ponikowski <piotr.ponikowski@source-corp.com>
2024-12-06 06:35:34 +0100

    day 6 tests

--- src/test/kotlin/Day6Test.kt
+++ src/test/kotlin/Day6Test.kt
@@ -8,21 +8,21 @@ class Day6Test : FunSpec({
 
     context("Part 1") {
         test("should solve example") {
-            Day6(exampleInput).part1() shouldBe 1
+            Day6(exampleInput).part1() shouldBe 41
         }
 
         test("should solve real input") {
-            Day6(realInput).part1() shouldBe 1
+            Day6(realInput).part1() shouldBe 4776
         }
     }
 
     context("Part 2") {
         test("should solve example") {
-            Day6(exampleInput).part2() shouldBe 2
+            Day6(exampleInput).part2() shouldBe 6
         }
 
         test("should solve real input") {
-            Day6(realInput).part2() shouldBe 2
+            Day6(realInput).part2() shouldBe 1586
         }
     }
 })
