5718163f69a7a478dc92868be72ed6a63717330e
Piotr Ponikowski <piotrponikowski87@gmail.com>
2024-12-11 19:01:44 +0100

    day 9 cleanup

--- src/test/kotlin/Day9Test.kt
+++ src/test/kotlin/Day9Test.kt
@@ -3,26 +3,26 @@ import io.kotest.matchers.shouldBe
 
 class Day9Test : FunSpec({
 
-    val realInput = readLines("day9.txt")
-    val exampleInput = readLines("day9.txt", true)
+    val realInput = readText("day9.txt")
+    val exampleInput = readText("day9.txt", true)
 
     context("Part 1") {
         test("should solve example") {
-            Day9(exampleInput).part1() shouldBe 1
+            Day9(exampleInput).part1() shouldBe 1928
         }
 
         test("should solve real input") {
-            Day9(realInput).part1() shouldBe 1
+            Day9(realInput).part1() shouldBe 6242766523059
         }
     }
 
     context("Part 2") {
         test("should solve example") {
-            Day9(exampleInput).part2() shouldBe 2
+            Day9(exampleInput).part2() shouldBe 2858
         }
 
         test("should solve real input") {
-            Day9(realInput).part2() shouldBe 2
+            Day9(realInput).part2() shouldBe 6272188244509
         }
     }
 })
