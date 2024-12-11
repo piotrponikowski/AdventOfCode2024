35bf6e2f7adb3ca13bacd302ec022b64761abf6d
Piotr Ponikowski <piotrponikowski87@gmail.com>
2024-12-11 19:01:45 +0100

    day 10 cleanup

--- src/main/kotlin/Day10.kt
+++ src/main/kotlin/Day10.kt
@@ -5,51 +5,29 @@ class Day10(input: List<String>) {
         .associate { (point, symbol) -> point to if (symbol == '.') null else symbol.toString().toInt() }
 
     private val startingPoints = board.filterValues { value -> value == 0 }.keys
-
     private val edges = listOf(Point(-1, 0), Point(1, 0), Point(0, -1), Point(0, 1))
-    
+
     fun part1() = startingPoints
-        .map { startingPoint -> paths(startingPoint) }
+        .map { startingPoint -> searchEndPoints(startingPoint) }
         .sumOf { endPoints -> endPoints.toSet().size }
 
     fun part2() = startingPoints
-        .map { startingPoint -> paths(startingPoint) }
+        .map { startingPoint -> searchEndPoints(startingPoint) }
         .sumOf { endPoints -> endPoints.size }
 
-    private fun paths(start: Point): List<Point> {
-        var currentPoints = listOf(start)
+    private fun searchEndPoints(start: Point) = (1..9)
+        .fold(listOf(start)) { points, expectedValue -> calculateNextPoints(points, expectedValue) }
 
-        (1..9).forEach { value ->
-            val nextPoints = mutableListOf<Point>()
-
-            currentPoints.forEach { point ->
-                edges.forEach { direction ->
-                    val neighbour = point + direction
-                    val edgeValue = board[neighbour]
-                    if (edgeValue == value) {
-                        nextPoints += neighbour
-                    }
-                }
+    private fun calculateNextPoints(points: List<Point>, expectedValue: Int) = points
+        .flatMap { point ->
+            edges.flatMap { direction ->
+                val neighbour = point + direction
+                val edgeValue = board[neighbour]
+                if (edgeValue == expectedValue) listOf(neighbour) else emptyList()
             }
-
-            currentPoints = nextPoints
         }
-
-        return currentPoints
-    }
 
     data class Point(val x: Int, val y: Int) {
         operator fun plus(other: Point) = Point(x + other.x, y + other.y)
     }
-}
-
-fun main() {
-    val realInput = readLines("day10.txt")
-    val exampleInput = readLines("day10.txt", true)
-
-    val r1 = Day10(exampleInput).part2()
-    println(r1)
-
-    val r2 = Day10(realInput).part2()
-    println(r2)
 }
