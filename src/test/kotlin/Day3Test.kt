bdbf05dd6ca7e812c12a3badb0414833b89b93a8
Piotr Ponikowski <piotrponikowski87@gmail.com>
2024-12-11 19:01:41 +0100

    day 3 tests

--- src/test/kotlin/Day3Test.kt
+++ src/test/kotlin/Day3Test.kt
@@ -8,21 +8,21 @@ class Day3Test : FunSpec({
     
     context("Part 1") {
         test("should solve example") {
-            Day3(exampleInput).part1() shouldBe 1
+            Day3(exampleInput).part1() shouldBe 161
         }
         
         test("should solve real input") {
-            Day3(realInput).part1() shouldBe 1
+            Day3(realInput).part1() shouldBe 170778545
         }
     }
 
     context("Part 2") {
         test("should solve example") {
-            Day3(exampleInput).part2() shouldBe 2
+            Day3(exampleInput).part2() shouldBe 48
         }
         
         test("should solve real input") {
-            Day3(realInput).part2() shouldBe 2
+            Day3(realInput).part2() shouldBe 82868252
         }
     }
 })
