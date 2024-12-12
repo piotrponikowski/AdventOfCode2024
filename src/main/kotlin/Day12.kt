1b6a234462a89152091b19cf5a547159364228c6
Piotr Ponikowski <piotrponikowski87@gmail.com>
2024-12-12 17:07:10 +0100

    day 12 cleanup

--- src/main/kotlin/Day12.kt
+++ src/main/kotlin/Day12.kt
@@ -3,17 +3,17 @@ class Day12(input: List<String>) {
     private val board = input
         .flatMapIndexed { y, line -> line.mapIndexed { x, symbol -> Point(x, y) to symbol } }.toMap()
 
-    fun part1() = allAreas().fold(0) { result, area -> result + (perimeter(area).size * area.size) }
-    
-    fun part2() = allAreas().fold(0) { result, area -> result + (corners(area).size * area.size) }
+    fun part1() = gatherAreas().fold(0) { result, area -> result + (computePerimeter(area).size * area.size) }
 
-    private fun allAreas(): List<Set<Point>> {
+    fun part2() = gatherAreas().fold(0) { result, area -> result + (computeCorners(area).size * area.size) }
+
+    private fun gatherAreas(): List<Set<Point>> {
         val visited = mutableSetOf<Point>()
         val result = mutableListOf<Set<Point>>()
 
         board.keys.forEach { point ->
             if (point !in visited) {
-                val area = area(point)
+                val area = computeArea(point)
                 visited += area
                 result.add(area)
             }
@@ -22,43 +22,7 @@ class Day12(input: List<String>) {
         return result
     }
 
-    private fun corners(area: Set<Point>): List<Point> = Direction.neighbours
-        .flatMap { (dir1, dir2) -> area.filter { point -> isCorner(point, dir1, dir2) } }
-
-//        result += area.filter { point -> isOpenCorner(point, Direction.U, Direction.L) }
-//        result += area.filter { point -> isOpenCorner(point, Direction.U, Direction.R) }
-//        result += area.filter { point -> isOpenCorner(point, Direction.D, Direction.L) }
-//        result += area.filter { point -> isOpenCorner(point, Direction.D, Direction.R) }
-//
-//        result += area.filter { point -> isClosedCorner(point, Direction.U, Direction.L) }
-//        result += area.filter { point -> isClosedCorner(point, Direction.U, Direction.R) }
-//        result += area.filter { point -> isClosedCorner(point, Direction.D, Direction.L) }
-//        result += area.filter { point -> isClosedCorner(point, Direction.D, Direction.R) }
-
-//        result += area.filter { point -> board[point + Direction.U] != board[point] && board[point + Direction.L] != board[point] }
-//        result += area.filter { point -> board[point + Direction.U] != board[point] && board[point + Direction.R] != board[point] }
-//        result += area.filter { point -> board[point + Direction.D] != board[point] && board[point + Direction.L] != board[point] }
-//        result += area.filter { point -> board[point + Direction.D] != board[point] && board[point + Direction.R] != board[point] }
-
-//        result += area.filter { point -> board[point + Direction.U + Direction.L] != board[point] && board[point + Direction.U] == board[point] && board[point + Direction.L] == board[point] }
-//        result += area.filter { point -> board[point + Direction.U + Direction.R] != board[point] && board[point + Direction.U] == board[point] && board[point + Direction.R] == board[point] }
-//        result += area.filter { point -> board[point + Direction.D + Direction.L] != board[point] && board[point + Direction.D] == board[point] && board[point + Direction.L] == board[point] }
-//        result += area.filter { point -> board[point + Direction.D + Direction.R] != board[point] && board[point + Direction.D] == board[point] && board[point + Direction.R] == board[point] }
-
-//        println(result)
-//        return result
-//    }
-
-    private fun isCorner(p: Point, dir1: Direction, dir2: Direction) =
-        isOpenCorner(p, dir1, dir2) || isClosedCorner(p, dir1, dir2)
-
-    private fun isOpenCorner(p: Point, dir1: Direction, dir2: Direction) =
-        board[p + dir1] != board[p] && board[p + dir2] != board[p]
-
-    private fun isClosedCorner(p: Point, dir1: Direction, dir2: Direction) =
-        board[p + dir1 + dir2] != board[p] && board[p + dir1] == board[p] && board[p + dir2] == board[p]
-
-    private fun area(startingPoint: Point): Set<Point> {
+    private fun computeArea(startingPoint: Point): Set<Point> {
         val result = mutableSetOf<Point>()
         val pointsToCheck = mutableListOf(startingPoint)
 
@@ -76,7 +40,7 @@ class Day12(input: List<String>) {
         return result
     }
 
-    private fun perimeter(areaPoints: Set<Point>): List<Point> {
+    private fun computePerimeter(areaPoints: Set<Point>): List<Point> {
         val perimeter = mutableListOf<Point>()
 
         areaPoints.forEach { point ->
@@ -89,6 +53,18 @@ class Day12(input: List<String>) {
         }
         return perimeter
     }
+
+    private fun computeCorners(area: Set<Point>): List<Point> = Direction.neighbours
+        .flatMap { (dir1, dir2) -> area.filter { point -> isCorner(point, dir1, dir2) } }
+
+    private fun isCorner(p: Point, dir1: Direction, dir2: Direction) =
+        isOpenCorner(p, dir1, dir2) || isClosedCorner(p, dir1, dir2)
+
+    private fun isOpenCorner(p: Point, dir1: Direction, dir2: Direction) =
+        board[p + dir1] != board[p] && board[p + dir2] != board[p]
+
+    private fun isClosedCorner(p: Point, dir1: Direction, dir2: Direction) =
+        board[p + dir1 + dir2] != board[p] && board[p + dir1] == board[p] && board[p + dir2] == board[p]
 
     data class Point(val x: Int, val y: Int) {
         operator fun plus(other: Direction) = Point(x + other.x, y + other.y)
