2325e55f27f125c1dbad4f92e1e6f5ad195abef3
Piotr Ponikowski <piotrponikowski87@gmail.com>
2024-12-16 17:56:12 +0100

    day 16 rework

--- src/main/kotlin/Day16.kt
+++ src/main/kotlin/Day16.kt
@@ -3,105 +3,65 @@ class Day16(input: List<String>) {
     private val board = input
         .flatMapIndexed { y, line -> line.mapIndexed { x, symbol -> Point(x, y) to symbol } }.toMap()
 
-    private val rotations = mapOf(
-        Direction.R to listOf(Direction.R, Direction.D, Direction.U),
-        Direction.L to listOf(Direction.L, Direction.D, Direction.U),
-        Direction.U to listOf(Direction.U, Direction.L, Direction.R),
-        Direction.D to listOf(Direction.D, Direction.L, Direction.R)
-    )
-    
-    fun part1() = solve().minOfOrNull { it.score }
+    private val startPosition = board.entries.first { (_, symbol) -> symbol == 'S' }.key
+    private val endPosition = board.entries.first { (_, symbol) -> symbol == 'E' }.key
 
-    fun part2() = solve().let {results-> 
-        val bestScore = results.map { it.score }.min()
-        results.filter { it.score == bestScore }.flatMap { it.states.map { it.position } }.toSet().size
-    }
+    fun part1() = findPaths().minOfOrNull { path -> path.score }
 
-    private fun solve():List<Path> {
+    fun part2() = findPaths()
+        .let { paths -> paths to paths.minOfOrNull { path -> path.score } }
+        .let { (paths, minScore) -> paths.filter { path -> path.score == minScore } }
+        .flatMap { path -> path.positions }.toSet().size
 
-        val start = board.entries.first { it.value == 'S' }.key
-        val end = board.entries.first { it.value == 'E' }.key
-
-        val startStates = listOf(Path(listOf(State(start, Direction.R)), 0, listOf(Debug(start, Direction.R, 0))))
-
-        var currentPaths = startStates
-        val result = mutableListOf<Path>()
+    private fun findPaths(): List<Path> {
+        val results = mutableListOf<Path>()
         val visited = mutableMapOf<State, Int>()
 
+        val startPath = Path(listOf(startPosition), Direction.R, 0)
+        val currentPaths = mutableListOf(startPath)
+
         while (currentPaths.isNotEmpty()) {
-            val nextPaths = mutableListOf<Path>()
+            val currentPath = currentPaths.removeFirst()
+            val currentPosition = currentPath.positions.last()
 
-            currentPaths.forEach { currentPath ->
-                val state = currentPath.states.last()
+            Direction.rotations(currentPath.direction).forEach { newDirection ->
+                val newPosition = currentPosition + newDirection
+                val newScore = currentPath.score + if (currentPath.direction == newDirection) 1 else 1001
+                val newPath = Path(currentPath.positions + newPosition, newDirection, newScore)
 
-                rotations[state.direction]!!.forEach { newDirection ->
-                    val newPosition = state.position + newDirection
-                    val newScore = currentPath.score + (if (state.direction == newDirection) 1 else 1001)
-                    val newState = State(newPosition, newDirection)
-                    val newDebug = Debug(newPosition, newDirection, newScore)
-                    val newPath = Path(currentPath.states + newState, newScore, currentPath.debug + newDebug)
+                val stateKey = State(newPosition, newDirection)
+                val stateScore = visited[stateKey] ?: Int.MAX_VALUE
 
-                    val validEnd = board[newPosition] == 'E'
-                    val validPosition = board[newPosition] == '.'
-                    val validPath = newState !in currentPath.states
-                    val validScore = newScore <= (visited[newState]?: Int.MAX_VALUE)
-
-                    
-                    if (validEnd) {
-                        result += newPath
-                    } else if (validPosition && validPath && validScore) {
-                        nextPaths += newPath
-                        visited[newState] = newScore
-                    }
+                if (newPosition == endPosition) {
+                    results += newPath
+                } else if (board[newPosition] == '.' && newScore <= stateScore) {
+                    currentPaths += newPath
+                    visited[stateKey] = newScore
                 }
             }
-
-            currentPaths = nextPaths.toList()
         }
 
-        return result
+        return results
     }
 
-    private fun printPath(path: Path) {
-        val minX = board.minOf { it.key.x }
-        val maxX = board.maxOf { it.key.x }
-        val minY = board.minOf { it.key.y }
-        val maxY = board.maxOf { it.key.y }
-
-        (minY..maxY).forEach { y ->
-            (minX..maxX).forEach { x ->
-                val point = Point(x, y)
-                val hasPoint = path.states.any { it.position == point }
-
-                print(if (hasPoint) 'P' else board[point])
-            }
-            println()
-        }
-    }
-
-    data class Path(val states: List<State>, val score: Int, val debug:List<Debug>)
+    data class Path(val positions: List<Point>, val direction: Direction, val score: Int)
 
     data class State(val position: Point, val direction: Direction)
-
-    data class Debug(val position: Point, val direction: Direction, val score:Int)
-
 
     data class Point(val x: Int, val y: Int) {
         operator fun plus(other: Direction) = Point(x + other.x, y + other.y)
     }
 
-    enum class Direction(val symbol: Char, val x: Int, val y: Int) {
-        L('<', -1, 0), R('>', 1, 0), U('^', 0, -1), D('v', 0, 1);
+    enum class Direction(val x: Int, val y: Int) {
+        L(-1, 0), R(1, 0), U(0, -1), D(0, 1);
+
+        companion object {
+            fun rotations(direction: Direction) = when (direction) {
+                R -> listOf(R, D, U)
+                L -> listOf(L, D, U)
+                U -> listOf(U, L, R)
+                D -> listOf(D, L, R)
+            }
+        }
     }
-}
-
-fun main() {
-    val realInput = readLines("day16.txt")
-    val exampleInput = readLines("day16-1.txt", true)
-
-    val r1 = Day16(exampleInput).part1()
-    println(r1)
-
-    val r2 = Day16(realInput).part1()
-    println(r2)
 }
