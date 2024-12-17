1381a259832e636ef36dd74e8d3668f3e7cc20fe
Piotr Ponikowski <piotrponikowski87@gmail.com>
2024-12-17 17:02:56 +0100

    day 17 tests

--- src/main/kotlin/Day17.kt
+++ src/main/kotlin/Day17.kt
@@ -3,85 +3,163 @@ import kotlin.math.pow
 
 class Day17(input: List<String>) {
 
-    private val registers = input.take(3).map { it.split(": ").last().toInt() }
-    private val instructions = input.last().split(": ").last().split(",").map { it.toInt() }
+    private val registers = input.take(3).map { it.split(": ").last().toLong() }
+    private val instructions = input.last().split(": ").last().split(",").map { it.toLong() }
 
-    fun part1() = solve(registers, instructions).joinToString(",")
+    fun part1() = solve(registers, instructions, registers[0]).joinToString(",")
+    
+    fun part2() = findOutput(instructions)
 
-    fun part2() {
-
-        var index = 0
-        while (true) {
-            if(index % 1000 == 0) {
-                println(index)
-            }
-            
-            val fixedRegisters = registers.toMutableList()
-            fixedRegisters[0] = index
-            val result = solve(fixedRegisters, instructions)
-            if (result == instructions) {
-                break
-            } else {
-                index++
-            }
+    private fun findOutput(target: List<Long>): Long {
+        var a = if (target.size == 1) {
+            0
+        } else {
+            8 * findOutput(target.subList(1, target.size))
         }
-        println(index)
+
+        while (solve(instructions, instructions, a) != target) {
+            a++
+        }
+
+        return a
     }
 
-    fun solve(registers: List<Int>, instructions: List<Int>): List<Int> {
+
+//    fun part2() {
+//
+////        var index1 = registers[0]
+////        while (true) {
+////            val fixedRegisters = registers.toMutableList()
+////            fixedRegisters[0] = index1
+////            val result = solve(fixedRegisters, instructions)
+////            if(result.size == instructions.size) {
+////                println(result)
+////                break
+////            } else {
+////                index1 *= 2
+////            }
+////        }
+////
+////        var index2 = index1
+////        while (true) {
+////            val fixedRegisters = registers.toMutableList()
+////            fixedRegisters[0] = index2
+////            val result = solve(fixedRegisters, instructions)
+////            if(result.take(6) == instructions.take(6)) {
+////                println(result)
+////                break
+////            } else {
+////                index2++
+////            }
+////        }
+////
+////        val diff1 = index2 - index1
+////        var index3 = index2
+////        while (true) {
+////            val fixedRegisters = registers.toMutableList()
+////            fixedRegisters[0] = index3
+////            val result = solve(fixedRegisters, instructions)
+////            if(result.take(7) == instructions.take(7)) {
+////                println(result)
+////                break
+////            } else {
+////                index3 += diff1
+////            }
+////        }
+//
+////        var index = 69402619166958
+////        var diff = 10000
+////        while (true) {
+////            val fixedRegisters = registers.toMutableList()
+////            fixedRegisters[0] = index
+////            val result = solve(fixedRegisters, instructions)
+////            if (result.take(9) == instructions.take(9)) {
+////                println(index)
+////                println(result)
+////                index += diff
+//////                break
+////            } else {
+////                index += diff
+////            }
+////        }
+//        
+//        var index = 69549419806958
+//        // to 69553177886958
+//        var diff = 100
+//
+//        while (true) {
+//            val fixedRegisters = registers.toMutableList()
+//            fixedRegisters[0] = index
+//            val result = solve(fixedRegisters, instructions)
+//            if (result.take(11) == instructions.take(11)) {
+//                println(result)
+//                println(index)
+//                break
+//            }  else {
+//                index += diff
+//            }
+//            
+//            if(index > 69553177886958) {
+//                break
+//            }
+//        }
+//    }
+
+    fun solve(registers: List<Long>, instructions: List<Long>, aStart: Long): List<Long> {
         val currentRegisters = registers.toMutableList()
+        currentRegisters[0] = aStart
         var pointer = 0
-        val output = mutableListOf<Int>()
+        val output = mutableListOf<Long>()
 
         while (pointer < instructions.size) {
             val instruction = instructions[pointer]
             val operand = instructions[pointer + 1]
             var jumped = false
 
-            if (instruction == 0) {
+            if (instruction == 0L) {
                 val numerator = currentRegisters[0]
                 val denominator = 2.0.pow(combo(operand, currentRegisters).toDouble()).toInt()
                 val result = numerator / denominator
                 currentRegisters[0] = result
                 //println("adv result = $result")
-            } else if (instruction == 1) {
+            } else if (instruction == 1L) {
                 val value = currentRegisters[1]
                 val result = value.xor(operand)
                 currentRegisters[1] = result
                 //println("bxl result = $result")
-            } else if (instruction == 2) {
+            } else if (instruction == 2L) {
                 val value = combo(operand, currentRegisters)
                 val result = value % 8
                 currentRegisters[1] = result
                 //println("bst result = $result")
-            } else if (instruction == 3) {
+            } else if (instruction == 3L) {
                 val value = currentRegisters[0]
 
-                if (value == 0) {
+                if (value == 0L) {
                     //println("jnz result = skipped")
                 } else {
-                    pointer = operand
+                    pointer = operand.toInt()
                     jumped = true
                     //println("jnz result = $operand")
                 }
-            } else if (instruction == 4) {
+            } else if (instruction == 4L) {
                 val value1 = currentRegisters[1]
                 val value2 = currentRegisters[2]
                 val result = value1.xor(value2)
                 currentRegisters[1] = result
                 //println("bxc result = $result")
-            } else if (instruction == 5) {
+            } else if (instruction == 5L) {
                 val value = combo(operand, currentRegisters)
                 val result = value % 8
                 output += result
                 //println("out result = $result")
-            } else if (instruction == 6) {
+            } else if (instruction == 6L) {
                 val numerator = currentRegisters[0]
-                val denominator = 2.0.pow(combo(operand, currentRegisters).toDouble()).toInt()
+                val denominator = 2.0.pow(combo(operand, currentRegisters).toDouble()).toLong()
                 val result = numerator / denominator
                 currentRegisters[1] = result
                 //println("bdv result = $result")
-            } else if (instruction == 7) {
+            } else if (instruction == 7L) {
                 val numerator = currentRegisters[0]
                 val denominator = 2.0.pow(combo(operand, currentRegisters).toDouble()).toInt()
                 val result = numerator / denominator
@@ -97,15 +175,15 @@ class Day17(input: List<String>) {
         return output
     }
 
-    private fun combo(operand: Int, registers: List<Int>) =
+    private fun combo(operand: Long, registers: List<Long>): Long =
         when (operand) {
-            0 -> operand
-            1 -> operand
-            2 -> operand
-            3 -> operand
-            4 -> registers[0]
-            5 -> registers[1]
-            6 -> registers[2]
+            0L -> operand
+            1L -> operand
+            2L -> operand
+            3L -> operand
+            4L -> registers[0]
+            5L -> registers[1]
+            6L -> registers[2]
             else -> throw IllegalArgumentException()
         }
 
--- src/test/kotlin/Day17Test.kt
+++ src/test/kotlin/Day17Test.kt
@@ -4,25 +4,26 @@ import io.kotest.matchers.shouldBe
 class Day17Test : FunSpec({
 
     val realInput = readLines("day17.txt")
-    val exampleInput = readLines("day17.txt", true)
+    val exampleInput1 = readLines("day17-1.txt", true)
+    val exampleInput2 = readLines("day17-2.txt", true)
 
     context("Part 1") {
         test("should solve example") {
-            Day17(exampleInput).part1() shouldBe 1
+            Day17(exampleInput1).part1() shouldBe "4,6,3,5,6,3,5,2,1,0"
         }
 
         test("should solve real input") {
-            Day17(realInput).part1() shouldBe 1
+            Day17(realInput).part1() shouldBe "2,3,6,2,1,6,1,2,1"
         }
     }
 
     context("Part 2") {
         test("should solve example") {
-            Day17(exampleInput).part2() shouldBe 2
+            Day17(exampleInput2).part2() shouldBe 117440L
         }
 
         test("should solve real input") {
-            Day17(realInput).part2() shouldBe 2
+            Day17(realInput).part2() shouldBe 90938893795561
         }
     }
 })
