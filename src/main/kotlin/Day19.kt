69622b95475cbda0863e43b25aa40de9b25ae871
Piotr Ponikowski <piotrponikowski87@gmail.com>
2024-12-22 19:28:41 +0100

    day 19 cleanup

--- src/main/kotlin/Day19.kt
+++ src/main/kotlin/Day19.kt
@@ -2,26 +2,20 @@ class Day19(input: List<List<String>>) {
 
     private val towels = input.first().first().split(", ")
     private val designs = input.last()
-    private val designCache = mutableMapOf<String, Long>()
 
     fun part1() = designs.count { design -> countDesigns(design) > 0 }
 
     fun part2() = designs.sumOf { design -> countDesigns(design) }
 
     private fun countDesigns(design: String): Long {
-        if (design.isEmpty()) return 1L
+        val countCache = mutableMapOf("" to 1L)
 
-        val cachedCount = designCache[design]
-        if (cachedCount != null) {
-            return cachedCount
+        fun count(designPart: String): Long = countCache.getOrPut(designPart) {
+            towels.filter { towel -> designPart.startsWith(towel) }
+                .map { towel -> designPart.removePrefix(towel) }
+                .sumOf { newDesign -> count(newDesign) }
         }
 
-        val count = towels
-            .filter { towel -> design.startsWith(towel) }
-            .map { towel -> design.drop(towel.length) }
-            .sumOf { newDesign -> countDesigns(newDesign) }
-
-        designCache[design] = count
-        return count
+        return count(design)
     }
 }
