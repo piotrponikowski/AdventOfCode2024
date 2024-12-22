bc80b8757758ac29d4c52545e68ca7b57a647a94
Piotr Ponikowski <piotrponikowski87@gmail.com>
2024-12-22 11:48:21 +0100

    day 22 cleanup

--- src/main/kotlin/Day22.kt
+++ src/main/kotlin/Day22.kt
@@ -2,80 +2,35 @@ class Day22(input: List<String>) {
 
     private val secrets = input.map { it.toLong() }
 
-    fun part1() {
-        val results = secrets.map { solve(it) }
-        println(results)
-        println(results.sum())
+    fun part1() = secrets.sumOf { secret -> calculateSecrets(secret).last() }
+
+    fun part2(): Long {
+        val buyTimes = secrets.map { secret -> calculateBuyTime(secret) }
+        val buySignals = buyTimes.flatMap { cache -> cache.keys }.toSet()
+        
+        return buySignals.maxOf { buySignal -> buyTimes.sumOf { cache -> cache[buySignal] ?: 0L } }
     }
 
-    fun part2()  {
-        val prices = secrets.map { prices(it) }
-        val priceChanges = prices.map { diffs(it) }
-        val priceChangesWindowed = priceChanges
-            .mapIndexed { index1, changes -> buildCache(prices[index1], changes) }
-        
-        val keys = priceChanges
-            .flatMap { result -> result.windowed(4) }
-            .map {it.joinToString("") { it.toString() } }
-            .toSet()
-        
-//        val keys = listOf(listOf(-1,-1,0,2).joinToString { it.toString("") })
-        
-        
-        var maxBananas  =0L
-        keys.forEachIndexed { i, key ->
-            println("Key $i of ${keys.size}")
-            var bananas = 0L
+    private fun calculateBuyTime(secret: Long): Map<String, Long> {
+        val prices = calculatePrices(secret)
+        val changes = calculateChanges(prices)
+        val result = mutableMapOf<String, Long>()
 
-            priceChangesWindowed.forEachIndexed { index, priceChange ->
-                val priceIndex = priceChange[key]
-                if(priceIndex != null) {
-                    bananas += priceIndex
-                }
-            }
-            
-            if(bananas > maxBananas) {
-                maxBananas = bananas
-                println("Current Result $maxBananas")
-            }
-        }
-        
-        
-        println("Result $maxBananas")
-    }
-    
-    private fun buildCache(prices: List<Int>, changes: List<Int>):Map<String, Int> {
-        val result = mutableMapOf<String, Int>()
-        val keys = changes.windowed(4).map { it.joinToString("") }
-        keys.forEachIndexed { index, key -> 
-            if(!result.containsKey(key)) {
-                result[key] =  prices[index + 4]
-            }
-        }
-        return result
-    }
-    
-    private fun diffs(prices: List<Int>) = prices.windowed(2).map { (a, b) -> b-a }
-
-    private fun prices(secret: Long): List<Int> {
-        var current = secret
-        val result = mutableListOf<Int>()
-        (0..<2000).forEach {
-            result += current.toString().last().toString().toInt()
-            current = nextSecret(current)
+        changes.windowed(4).mapIndexed { index, lastChanges ->
+            result.computeIfAbsent(lastChanges.joinToString()) { prices[index + 4] }
         }
 
         return result
     }
-    
-    private fun solve(secret: Long): Long {
-        var result = secret
-        (0..<2000).forEach {
-            result = nextSecret(result)
-        }
 
-        return result
-    }
+    private fun calculateChanges(prices: List<Long>) = prices
+        .windowed(2).map { (pricePrev, priceNext) -> priceNext - pricePrev }
+
+    private fun calculatePrices(secret: Long) = calculateSecrets(secret)
+        .map { nextSecret -> nextSecret.toString().last().toString().toLong() }
+
+    private fun calculateSecrets(secret: Long) = (0..<2000)
+        .runningFold(secret) { currentSecret, _ -> nextSecret(currentSecret) }
 
     private fun nextSecret(secret: Long): Long {
         var result = prune(mix(secret, secret * 64))
@@ -87,18 +42,4 @@ class Day22(input: List<String>) {
     private fun mix(secret: Long, value: Long) = secret.xor(value)
 
     private fun prune(secret: Long) = secret.mod(16777216L)
-    
-}
-
-fun main() {
-    val realInput = readLines("day22.txt")
-    val exampleInput = readLines("day22.txt", true)
-
-    val r1 = Day22(exampleInput).part2()
-    println(r1)
-
-    val r2 = Day22(realInput).part2()
-    println(r2)
-    
-    //1570
 }
--- src/test/kotlin/Day22Test.kt
+++ src/test/kotlin/Day22Test.kt
@@ -4,25 +4,26 @@ import io.kotest.matchers.shouldBe
 class Day22Test : FunSpec({
 
     val realInput = readLines("day22.txt")
-    val exampleInput = readLines("day22.txt", true)
+    val exampleInput1 = readLines("day22-1.txt", true)
+    val exampleInput2 = readLines("day22-2.txt", true)
 
     context("Part 1") {
         test("should solve example") {
-            Day22(exampleInput).part1() shouldBe 1
+            Day22(exampleInput1).part1() shouldBe 37327623
         }
 
         test("should solve real input") {
-            Day22(realInput).part1() shouldBe 1
+            Day22(realInput).part1() shouldBe 13753970725
         }
     }
 
     context("Part 2") {
         test("should solve example") {
-            Day22(exampleInput).part2() shouldBe 2
+            Day22(exampleInput2).part2() shouldBe 23
         }
 
         test("should solve real input") {
-            Day22(realInput).part2() shouldBe 2
+            Day22(realInput).part2() shouldBe 1570
         }
     }
 })
