aa17e82f86aa7f1cd29909c4a308b6b052c6d7ab
Piotr Ponikowski <piotrponikowski87@gmail.com>
2024-12-23 11:25:18 +0100

    day 23 cleanup

--- src/main/kotlin/Day23.kt
+++ src/main/kotlin/Day23.kt
@@ -6,47 +6,47 @@ class Day23(input: List<String>) {
         .groupBy({ (computer1, _) -> computer1 }, { (_, computer2) -> computer2 })
         .toMap()
 
-    fun part1() = allGroups()
+    fun part1() = groups()
         .filter { group -> group.size == 3 }
-        .toSet()
         .count { group -> group.any { computer -> computer.startsWith("t") } }
 
-    fun part2() = allGroups()
+    fun part2() = groups()
         .maxBy { group -> group.size }
         .sorted()
         .joinToString(",")
 
-    fun allGroups() = computers.keys
-        .fold(emptyList<Set<String>>()) { result, computer -> result + groups(computer) }
+    
+    private fun groups(): Set<Set<String>> {
+        val result = mutableSetOf<Set<String>>()
 
-    var index = 0
-    private fun groups(computer: String): List<Set<String>> {
-        println(index++)
-        val connectedComputers = computers[computer]!!
-        val cache = computers.filterKeys { computer2 -> computer2 in connectedComputers }
+        computers.keys.forEach { computer ->
+            val connections = computers[computer]!!
+            
+            val startGroup = setOf(computer)
+            var currentGroups = setOf(startGroup)
+            result += startGroup
 
-        val result = mutableListOf(setOf(computer))
-        var currentGroups = setOf(setOf(computer))
-
-        while (currentGroups.isNotEmpty()) {
-            val nextGroups = mutableSetOf<Set<String>>()
-
-            currentGroups.forEach { currentGroup ->
-                connectedComputers.forEach { nextComputer ->
-                    if (nextComputer !in currentGroup) {
-                        val nextConnections = cache[nextComputer]!!
-                        if (nextConnections.containsAll(currentGroup)) {
-                            val nextGroup = currentGroup + nextComputer
-                            if (nextGroup !in nextGroups) {
-                                nextGroups += nextGroup
+            while (currentGroups.isNotEmpty()) {
+                val nextGroups = mutableSetOf<Set<String>>()
+                
+                currentGroups.forEach { currentGroup ->
+                    connections.forEach { nextComputer ->
+                        if (nextComputer !in currentGroup) {
+                            val otherConnections = computers[nextComputer]!!
+                            if (otherConnections.containsAll(currentGroup)) {
+                                val nextGroup = currentGroup + nextComputer
+                                
+                                if (nextGroup !in nextGroups) {
+                                    nextGroups += nextGroup
+                                }
                             }
                         }
                     }
                 }
+                
+                result += nextGroups
+                currentGroups = nextGroups
             }
-
-            result += nextGroups
-            currentGroups = nextGroups
         }
 
         return result
@@ -60,9 +60,6 @@ fun main() {
 //    val r1 = Day23(exampleInput).part2()
 //    println(r1)
 
-    val r2 = Day23(realInput).allGroups().groupBy {it}
-        .map { (a, b) -> a to b.size }
-        .sortedBy { it.second }
-        .forEach { println(it) }
+    val r2 = Day23(realInput).part2()
     println(r2)
 }
