7e4261be22b860874d31d2abda56f5227c272c49
Piotr Ponikowski <piotrponikowski87@gmail.com>
2024-12-25 06:27:25 +0100

    day 25 cleanup

--- src/main/kotlin/Day25.kt
+++ src/main/kotlin/Day25.kt
@@ -1,20 +1,18 @@
 class Day25(input: List<List<String>>) {
 
-    private val locks = input.filter { data -> isLock(data) }.map { data -> parse(data) }
-    private val keys = input.filter { data -> isKey(data) }.map { data -> parse(data) }
+    private val locks = input.filter { data -> isLock(data) }.map { data -> parsePins(data) }
+    private val keys = input.filter { data -> isKey(data) }.map { data -> parsePins(data) }
 
     fun part1() = locks.sumOf { lock -> keys.count { key -> isFit(lock, key) } }
 
     private fun isFit(lock: List<Int>, key: List<Int>) =
-        lock.zip(key).all { (lockPin, keyPin) -> lockPin + keyPin <= 5 }
+        lock.zip(key).all { (lockPins, keyPins) -> lockPins + keyPins <= 5 }
 
     private fun isLock(input: List<String>) = input.first().all { symbol -> symbol == '#' }
 
     private fun isKey(input: List<String>) = input.last().all { symbol -> symbol == '#' }
 
-    private fun parse(input: List<String>) = input
-        .flatMapIndexed { y, line -> line.mapIndexed { x, symbol -> Point(x, y) to symbol } }.toMap()
-        .let { data -> (0..4).map { x -> data.count { (point, symbol) -> point.x == x && symbol == '#' } - 1 } }
-    
+    private fun parsePins(input: List<String>) = (0..4).map { x -> input.count { line -> line[x] == '#' } - 1 }
+
     data class Point(val x: Int, val y: Int)
 }
