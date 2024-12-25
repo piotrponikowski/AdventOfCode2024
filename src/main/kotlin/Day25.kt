e8a978620e20f2462a7d2a93cd7e62030bc96a1c
Piotr Ponikowski <piotrponikowski87@gmail.com>
2024-12-25 06:21:09 +0100

    day 25

--- src/main/kotlin/Day25.kt
+++ src/main/kotlin/Day25.kt
@@ -1,60 +1,21 @@
 class Day25(input: List<List<String>>) {
 
-    private val locks = input.filter { isLock(it) }.map { parseLock(it) }
-    private val keys = input.filter { isKey(it) }.map { parseKey(it) }
+    private val locks = input.filter { data -> isLock(data) }.map { data -> parse(data) }
+    private val keys = input.filter { data -> isKey(data) }.map { data -> parse(data) }
 
-
-    fun part1(): Int {
-        println(locks.size)
-        println(keys.size)
-
-        println(locks)
-        println(keys)
-
-        var result = 0
-        locks.forEach { lock ->
-            keys.forEach { key ->
-
-                if (isFit(lock, key)) {
-
-                    result++
-                }
-            }
-        }
-
-        return result
-
-    }
+    fun part1() = locks.sumOf { lock -> keys.count { key -> isFit(lock, key) } }
 
     private fun isFit(lock: List<Int>, key: List<Int>) =
-        lock.zip(key).all { (a, b) -> a + b <= 5 }
+        lock.zip(key).all { (lockPin, keyPin) -> lockPin + keyPin <= 5 }
 
+    private fun isLock(input: List<String>) = input.first().all { symbol -> symbol == '#' }
 
-    private fun isLock(input: List<String>) = input.first().all { it == '#' }
+    private fun isKey(input: List<String>) = input.last().all { symbol -> symbol == '#' }
 
-    private fun isKey(input: List<String>) = input.last().all { it == '#' }
-
-    private fun parseLock(input: List<String>): List<Int> {
+    private fun parse(input: List<String>): List<Int> {
         val data = input.flatMapIndexed { y, line -> line.mapIndexed { x, symbol -> Point(x, y) to symbol } }.toMap()
-        return (0..4).map { x -> data.count { it.key.x == x && it.value == '#' } - 1 }
+        return (0..4).map { x -> data.count { (point, symbol) -> point.x == x && symbol == '#' } - 1 }
     }
-
-    private fun parseKey(input: List<String>): List<Int> {
-        val data = input.flatMapIndexed { y, line -> line.mapIndexed { x, symbol -> Point(x, y) to symbol } }.toMap()
-        return (0..4).map { x -> data.count { it.key.x == x && it.value == '#' } - 1 }
-    }
-
 
     data class Point(val x: Int, val y: Int)
-}
-
-fun main() {
-    val realInput = readGroups("day25.txt")
-    val exampleInput = readGroups("day25.txt", true)
-
-    val r1 = Day25(exampleInput).part1()
-    println(r1)
-
-    val r2 = Day25(realInput).part1()
-    println(r2)
 }
